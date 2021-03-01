e48d89d630e73fe0bd7d349dffb8b2b2f044bbad
Luis <luisr.felce@gmail.com>
2021-03-02 00:41:27 +0100

    Ionic-App

---
+++ src/app/models/lista.item.models.ts
@@ -0,0 +1,12 @@
+
+export class ListaItem {
+
+    desc      : string;
+    compleados: boolean;
+
+    constructor( desc: string) {
+
+        this.desc = desc;
+        this.compleados = false;
+    }
+}
